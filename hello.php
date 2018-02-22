15142b86673226f51d33e2c098b4ffd5a81afe79
rahulpagaria <rahulpagaria@gmail.com>
2018-02-22 05:50:15 -0500

    22feb

---
+++ hello.php
@@ -0,0 +1,26 @@
+<html>
+  <title> my page</title>
+</html>
+<h1>
+Movie Images
+</h1>
+<img src="download.png" width = 240>
+<br />
+<?php
+echo 'Hi !';
+  echo 'My name is index1.php !';
+$name = 'Rah';
+ echo 'My name is nospace' .$name. '<br />';
+
+if(1==1){echo 'ok';}
+
+$auth = true; //boolean
+ $age = 99; //integer
+ $temp = 60.89; //floating point
+ $identity = 'John Smith'; //string
+ $pasta = array('spaghetti', 'penne'); //array
+
+
+echo 'My obj is ' .$pasta[1]. 'hi' .$age. '<br />';
+
+?>
